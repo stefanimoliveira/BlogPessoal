1ce058fe2e997ad019ee293480738bd03a2a8f8b
unknown <stefanymoliveira87@gmail.com>
2022-06-18 16:56:39 -0300

    Criando SwaggerConfig

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -9,10 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name="tb_usuario")
@@ -26,8 +29,9 @@ public class Usuario {
 	@Size(min = 2, max = 100)
 	private String nome;
 	
-	@NotNull
-	@Size(min = 5, max = 100)
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 	
 	@NotNull
